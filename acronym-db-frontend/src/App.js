ff07765f04c04c80c3227635a5aa1ff08dfeaa78
Katia Gilligan <katia.gilligan88@gmail.com>
2020-05-25 21:14:43 -0700

    Created Full UI, all API requests work

--- acronym-db-frontend/src/App.js
+++ acronym-db-frontend/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import axios from "axios";
+import 'react-bootstrap-table/css/react-bootstrap-table.css'
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { DefaultButton } from 'office-ui-fabric-react';
-import './App.css';
+var ReactBsTable  = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
 const style = {
   container: {
@@ -18,7 +22,8 @@ const style = {
   }, 
   button: {
     color: "white", 
-    backgroundColor: "blue"
+    backgroundColor: "blue",
+    marginBottom: "50px"
   }
 }
 
@@ -30,17 +35,68 @@ class App extends React.Component {
       description: ""
     }
   }
+
+  handleChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };  
+
+  onSubmitHandler = event => {
+    const newAcronym = {
+      name: this.state.name,
+      description: this.state.description, 
+    };
+
+    axios
+      .post("http://localhost:6500/api/acronymNames", newAcronym)
+      .then(response => {
+        axios.get("http://localhost:6500/api/acronymNames").then(response => {
+      this.setState({
+        acronymData: response.data
+      })
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount = () => {
+    axios.get("http://localhost:6500/api/acronymNames").then(response => {
+      this.setState({
+        acronymData: response.data
+      })
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+
   render(){
     return (
       <div >
         <div style = {style.container} >
           <h1 style = {{margin: "0 auto", alignItems: "center"}} >Acronym Database</h1>
           <header style = {{marginBottom: "20px"}}><strong>Add A New Acronym</strong></header>
-          <TextField label="Acronym Name" placeholder="Enter Acronym Here" />
-          <TextField label="Description" multiline rows={3} />
+          <TextField  onChange={this.handleChangeHandler} name="name" label="Acronym Name" placeholder="Enter Acronym Here" />
+          <TextField  onChange={this.handleChangeHandler} name= "description" label="Description" multiline rows={3} />
           <br></br>
-          <DefaultButton style = {style.button} text="Add to Database" />
+          <DefaultButton  onClick={this.onSubmitHandler} style = {style.button} text="Add to Database" />
+          <BootstrapTable data={this.state.acronymData} striped hover>
+            <TableHeaderColumn isKey dataField='id'> ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='name'>Acronym Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='description'>Acronym Description</TableHeaderColumn>
+          </BootstrapTable>
+
         </div>
+        
       </div>
     );
   }
